a9a2b59d11b80fe6d49d37cb6e4094f155101503
Yusuf Kaba <kaba1729.ai@gmail.com>
2018-11-02 15:54:22 +0000

    - async in redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import configureStore from './store/configureStore';
+import {loadCourses} from "./actions/courseActions";
 import {Provider} from 'react-redux';
 import routes from './routes';
 import './styles/styles.css'; // Webpack can import css files too
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
