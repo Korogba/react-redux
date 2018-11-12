3f1d4d83028769d3587f94cd6eec9ca68f219bb6
Yusuf Kaba <kaba1729.ai@gmail.com>
2018-11-12 07:56:59 +0000

    - async status and error handling

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {Provider} from 'react-redux';
 import routes from './routes';
 import './styles/styles.css'; // Webpack can import css files too
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
 
 const store = configureStore();
 store.dispatch(loadCourses());
